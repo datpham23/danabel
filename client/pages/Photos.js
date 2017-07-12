d88290a1dbeb292caf56c22008bb8f3f64b1d701
Dat Pham <dpham@apple.com>
2017-07-11 23:21:33 -0700

    add more photos

--- client/pages/Photos.js
+++ client/pages/Photos.js
@@ -2,16 +2,31 @@ import React, { Component } from 'react';
 import '../sass/photos.scss';
 import Gallery from 'react-grid-gallery';
 
-const images = [];
-for (var i=1; i<16; i ++)
-  images.push({
-    src: `/images/${i}.jpg`,
-    thumbnail : `/images/${i}.jpg`
-  });
-
-
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      images : []
+    };
+  }
+
+  componentDidMount() {
+    (async ()=>{
+      let response = await fetch('/gallery/images');
+      let images = await response.json();
+      this.setState({
+        images : images
+      });
+    })();
+  }
   render() {
+    let images = this.state.images.map(imageName=>{
+      return {
+        src: `/images/gallery/${imageName}`,
+        thumbnail : `/images/gallery/${imageName}`
+      };
+    });
     return (
       <div className='photos-page'>
         <div style={{
